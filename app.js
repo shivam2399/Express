03b676eb4df10e57dcd8e1ef53e3b2a96354f615
shivam2399 <dubey.shivam2399@gmail.com>
2025-07-06 22:06:32 +0530

    Middleware implementation

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
+const addUser = (req, res, next) => {
+    req.user = "Guest";
+    next();
+}
+
+app.get('/welcome', addUser, (req, res) => {
+    res.send(`<h1>Welcome, ${req.user}!</h1>`);
+})
+
 app.listen(3000, () => {
     console.log('Server is up and running on port 3000! Ready to handle requests.');
 });
