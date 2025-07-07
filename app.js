6be14167c35bf38aef6486d6d901ea2429587c32
shivam2399 <dubey.shivam2399@gmail.com>
2025-07-07 09:51:08 +0530

    Express server with multiple routes

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const app = express();
+const port = 3000
 
-const addUser = (req, res, next) => {
-    req.user = "Guest";
-    next();
-}
+app.use(express.json());
 
-app.get('/welcome', addUser, (req, res) => {
-    res.send(`<h1>Welcome, ${req.user}!</h1>`);
+app.get('/orders', (req, res) => {
+    res.send('Here is the list of all orders');
 })
 
+app.post('/orders', (req, res) => {
+    res.send('A new order has been created');
+})
+
+app.get('/users', (req, res) => {
+    res.send('Here is the list of all users');
+})
+
+app.post('/users', (req, res) => {
+    res.send('A new user has been created');
+})
+
+
+
 app.listen(3000, () => {
-    console.log('Server is up and running on port 3000! Ready to handle requests.');
+    console.log(`Server is up and running on port ${port}!.`);
 });
