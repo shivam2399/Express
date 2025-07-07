313f38960886a704c045e497330ee2c0e44b7b15
shivam2399 <dubey.shivam2399@gmail.com>
2025-07-07 21:47:25 +0530

    Handling undefined routes

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
 const express = require('express');
 const app = express();
-const port = 3000
+const port = 4000
 
 app.use(express.json());
 
-app.get('/orders', (req, res) => {
-    res.send('Here is the list of all orders');
+app.get('/products', (req, res) => {
+    res.send('Here is the list of all products');
 })
 
-app.post('/orders', (req, res) => {
-    res.send('A new order has been created');
+app.post('/products', (req, res) => {
+    res.send('A new product has been added');
 })
 
-app.get('/users', (req, res) => {
-    res.send('Here is the list of all users');
+app.get('/categories', (req, res) => {
+    res.send('Here is the list of all categories');
 })
 
-app.post('/users', (req, res) => {
-    res.send('A new user has been created');
+app.post('/categories', (req, res) => {
+    res.send('A new category has been created');
+})
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>404- Page Not Found</h1>');
 })
 
 
-
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Server is up and running on port ${port}!.`);
 });
