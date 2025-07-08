41fb79ea5ddf90f28fa1e708f54fdaa5f61210c9
shivam2399 <dubey.shivam2399@gmail.com>
2025-07-08 09:29:22 +0530

    Using Routes for separation of routes

--- app.js
+++ app.js
@@ -1,31 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 4000
+const port = 3000
+const ordersRouter = require('./routes/orders');
+const usersRouter = require('./routes/users');
 
-app.use(express.json());
-
-app.use((req, res, next) => {
-    console.log(`${req.method} request made to '${req.url}'`);
-    next();
-})
-
-app.get('/products', (req, res) => {
-    res.send('Here is the list of all products');
-})
-
-app.post('/products', (req, res) => {
-    res.send('A new product has been added');
-})
-
-app.get('/categories', (req, res) => {
-    res.send('Here is the list of all categories');
-})
-
-app.post('/categories', (req, res) => {
-    res.send('A new category has been created');
-})
-
-
+app.use('/orders', ordersRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}!.`);
---
+++ routes/users.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('User List');
+});
+
+
+router.post('/', (req, res) => {
+    res.send('User Created');
+});
+
+module.exports = router;
+
