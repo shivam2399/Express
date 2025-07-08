2ab55db782686a2c234c60a264e8843ff69672b5
shivam2399 <dubey.shivam2399@gmail.com>
2025-07-08 09:10:10 +0530

    Middleware for logging HTTP method and endpoint

--- app.js
+++ app.js
@@ -4,42 +4,27 @@ const port = 4000
 
 app.use(express.json());
 
-// app.get('/products', (req, res) => {
-//     res.send('Here is the list of all products');
-// })
-
-// app.post('/products', (req, res) => {
-//     res.send('A new product has been added');
-// })
-
-// app.get('/categories', (req, res) => {
-//     res.send('Here is the list of all categories');
-// })
-
-// app.post('/categories', (req, res) => {
-//     res.send('A new category has been created');
-// })
-
-// app.get('/contact/:contactId', (req, res) => {
-//     const contactId = req.params.contactId;
-//     res.send(`Contact ID is: ${contactId}`);
-// })
-
-
-app.get('/welcome/:username', (req, res) => {
-    const username = req.params.username;   
-    const role = req.query.role;
-
-    if (role) {
-        res.send(`Welcome ${username}, your role is ${role}`);
-    } else {
-        res.send(`Welcome ${username}`);
-    }
+app.use((req, res, next) => {
+    console.log(`${req.method} request made to '${req.url}'`);
+    next();
 })
 
-app.all('*', (req, res) => {
-    res.status(404).send('<h1>404- Page Not Found</h1>');
+app.get('/products', (req, res) => {
+    res.send('Here is the list of all products');
 })
+
+app.post('/products', (req, res) => {
+    res.send('A new product has been added');
+})
+
+app.get('/categories', (req, res) => {
+    res.send('Here is the list of all categories');
+})
+
+app.post('/categories', (req, res) => {
+    res.send('A new category has been created');
+})
+
 
 
 app.listen(port, () => {
